4b01ed3edf47fa5a286079c127f16f2c9f832ef9
Fred Mitchell <lordalveric@yahoo.com>
2014-11-15 13:49:05 +0100

    pdns container works

--- dockers/build-shard-images.sh
+++ dockers/build-shard-images.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Build the containers for sharding (need only be run once)
 
+docker build -t pdns --rm=true pdns
 docker build -t mongo/basic --rm=true mongodb
 docker build -t mongo/shard-s --rm=true shard-s
 docker build -t mongo/shard-config --rm=true shard-config
