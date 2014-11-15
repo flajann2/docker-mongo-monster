f73723691240edd150e6c930c3bb4c194f878c88
Fred Mitchell <lordalveric@yahoo.com>
2014-11-15 11:37:29 +0100

    in progress on supervisor

--- dockers/pdns/rcmd/request.rb
+++ dockers/pdns/rcmd/request.rb
@@ -1,12 +1,21 @@
 #!/usr/bin/env ruby
 # Simple add / delete name to name server.
 
+=begin rdoc
+=NOTE WELL
+This script is not fool-proof as it does
+no sanity checking, so it is quite possible to pollute the DNS
+namespace with garbage if you are so inclined.
+=end
+
 # To get around Sinatra's annoying Rack protections, I had
 # to put these lines here like this. In this way the database
 # is opened BEFORE the Sinatra cops get here!!!!!
 require 'sqlite3'
 include SQLite3
 @@dbcon = Database.open "/db/pdns.db"
+
+# Nothing to see here. Move it along. 
 
 require 'sinatra'
 
@@ -27,17 +36,39 @@ end
 
 get '/status' do
   dbase do |db|
-    db.prepare(%{SELECT r.name, r.content
-                 FROM records AS r JOIN domains AS d ON (r.domain_id = d.id)  
-                 WHERE d.name = "srv" AND r.type = 'A'
+    db.prepare(%{
+                   SELECT r.name, r.content
+                   FROM records AS r JOIN domains AS d ON (r.domain_id = d.id)  
+                   WHERE d.name = "srv" AND r.type = 'A'
                 }).execute.map{ |row| row.join('-->')}.join('<br>')
   end
 end
 
 post '/name/:name/ip/:ip' do
-  "Adding / modifying name is #{params[:name]} and the ip is #{params[:ip]}"
+  name = params[:name]
+  ip = params[:ip]
+  dbase do |db|
+    db.execute(%{
+                  REPLACE INTO records 
+                         (domain_id, name,     type, ttl, content)
+                  SELECT  d.id,     '#{name}', 'A',   60, '#{ip}'
+                    FROM domains AS d 
+                       LEFT JOIN records AS r 
+                         ON (d.id = r.domain_id AND r.name = '#{name}' AND r.type = 'A')
+                    WHERE d.name = 'srv'
+                })
+  end
+  "Added / modifeid name is #{name} and the ip is #{ip}"
 end
 
-delete '/name/:name/ip/:ip' do
+delete '/name/:name' do
+  name = params[:name]
+  dbase do |db|
+    db.execute(%{
+                  DELETE FROM records
+                    WHERE name = '#{name}'
+                          AND type = 'A'
+                })
+  end
   "Deleting name is #{params[:name]} and the ip is #{params[:ip]}"
 end
