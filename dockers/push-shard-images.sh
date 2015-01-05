6d942ae905cea90db52dc978bc6acd4b791109b5
Fred Mitchell <lordalveric@yahoo.com>
2015-01-05 17:39:11 +0100

    images push to private repo

---
+++ dockers/push-shard-images.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Push images to our local repo
+
+REPO="docker-repo.srv:2061"
+
+function tag_and_push {
+    tag=$1
+    echo "Tag and pushing $REPO/$tag"
+    docker tag $(docker images -q $tag) $REPO/$tag
+    docker push $REPO/$tag
+}
+
+tag_and_push pdns
+tag_and_push tokumx/basic
+tag_and_push tokumx/toks-s
+tag_and_push tokumx/toks-config
+tag_and_push tokumx/toks-rep
