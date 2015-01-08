51e67569ad2d33397fd6ed9a409b6fdaf114bf39
Fred Mitchell <lordalveric@yahoo.com>
2015-01-08 17:56:18 +0100

    aaatoku cluster list

--- dockers/add-shards.js
+++ dockers/add-shards.js
@@ -1,6 +1,6 @@
-sh.addShard("afoo-shs1:28000");
-sh.addShard("afoo-shs2:28000");
-sh.addShard("afoo-shs1:28001");
-sh.addShard("afoo-shs0:28000");
-sh.addShard("afoo-shs2:28001");
-sh.addShard("afoo-shs0:28001");
+sh.addShard("aaatoku-shs2:28000");
+sh.addShard("aaatoku-shs2:28001");
+sh.addShard("aaatoku-shs1:28000");
+sh.addShard("aaatoku-shs0:28000");
+sh.addShard("aaatoku-shs1:28001");
+sh.addShard("aaatoku-shs0:28001");
